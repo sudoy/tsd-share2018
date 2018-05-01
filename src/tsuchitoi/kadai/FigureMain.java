8d48ba4267f72ea6387717951acba208bee49da3
tsuchitoi.keita <tsuchitoi.keita@tsd-201601-0004>
2018-05-01 16:28:14 +0900

    Squareを追加

--- src/tsuchitoi/kadai/FigureMain.java
+++ src/tsuchitoi/kadai/FigureMain.java
@@ -5,6 +5,12 @@ public class FigureMain {
 	public static void main(String[] args) {
 		Circle c1 = new Circle(10);
 		System.out.println(c1.getArea());
+		Square s1 = new Square(10);
+		System.out.println(s1.getArea());
+		Square s2 = new Square(20);
+		System.out.println(s2.getArea());
+
+
 	}
 
 }
@@ -22,5 +28,22 @@ class Circle{
 	}
 	public double getArea() {
 		return radius * radius * 3.14;
+	}
+}
+class Square{
+	private int side;
+
+	public Square(int side) {
+		super();
+		this.side = side;
+	}
+	public void setSide(int side) {
+		this.side = side;
+	}
+	public int getside() {
+		return side;
+	}
+	public int getArea() {
+		return side * side;
 	}
 }
