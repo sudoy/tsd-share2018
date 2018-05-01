da15fb8b85c7afa91405d4a5575c666f3b5fb580
N_F <N_F@DESKTOP-6RD84GH>
2018-05-01 16:27:14 +0900

    問題02（1～9まで）

---
+++ src/nakamura/kadai/FigureMain.java
@@ -0,0 +1,48 @@
+package nakamura.kadai;
+
+public class FigureMain {
+
+	public static void main(String[] args) {
+
+		// 円の面積
+		Circle maru = new Circle(5);
+		System.out.println("円の面積：" + maru.getArea());
+
+		// 正方形の面積
+		Square hako = new Square(10);
+		System.out.println("正方形の面積：" + hako.getArea());
+	}
+
+}
+
+class Circle {
+	private int radius;
+
+	public Circle(int radius) {
+		this.radius = radius;
+	}
+
+	public int getRadius() {
+		return radius;
+	}
+
+	public double getArea() {
+		return radius * radius * 3.14;
+	}
+}
+
+class Square {
+	private int side;
+
+	public Square(int side) {
+		this.side = side;
+	}
+
+	public int getSide() {
+		return side;
+	}
+
+	public int getArea() {
+		return side * side;
+	}
+}
