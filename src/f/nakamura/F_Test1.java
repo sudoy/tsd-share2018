d5d521177a32279672c208c8dcb637ecd10bb05b
N_F <N_F@DESKTOP-6RD84GH>
2018-05-01 15:41:24 +0900

    中村の新規ファイル

--- src/f/nakamura/F_Test1.java
+++ src/f/nakamura/F_Test1.java
@@ -1,9 +1,9 @@
-package f.Nakamura;
+package f.nakamura;
 
 public class F_Test1 {
 
 	public static void main(String[] args) {
-		System.out.println("Test");
+		System.out.println("Yes!!");
 	}
 
 }
