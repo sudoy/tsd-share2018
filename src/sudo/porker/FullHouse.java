ecef32757d7de2ba3ecab4ed259912e9215d7bba
kanauchi <kanauchi@tsd-201601-0009>
2018-05-02 16:12:54 +0900

    a

--- src/sudo/porker/FullHouse.java
+++ src/sudo/porker/FullHouse.java
@@ -6,14 +6,42 @@ public class FullHouse extends Hand {
 
 	@Override
 	public boolean hasRole(List<Card> cards) {
-		// TODO 自動生成されたメソッド・スタブ
-		return false;
+		int pair = 0;
+
+		for(Card c1 : cards) {
+			for(Card c2 : cards) {
+				if(c1.getMark().equals(c2.getMark()) &&
+						c1.getNumber() == c2.getNumber()) {
+					continue;
+				}
+
+				//同じカードが３枚あるか判定
+//				if(threeCard < 6) {
+//					if(c1.getNumber() == c2.getNumber()) {
+//						threeCard++;
+//						continue;
+//					}
+//				}
+//
+//				//同じカードが２枚あるか判定
+//				if(c1.getNumber() == c2.getNumber()) {
+//					pairCount++;
+//					continue;
+//				}
+
+				//ペアが8枚あればフルハウス ワンペア＝2 スリーカード=6
+				if(c1.getNumber() == c2.getNumber()) {
+					pair++;
+				}
+			}
+		}
+		return pair == 8;
+
 	}
 
 	@Override
 	public String getRoleName() {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
+		return "フルハウス";
 	}
 
 }
